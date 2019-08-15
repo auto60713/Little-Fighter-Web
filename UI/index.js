230814e37dbcc9e0e1395a53847a61f4e6a76387
Ozen <komicabot@gmail.com>
2019-08-15 15:14:04 +0800

    調整架構

---
+++ UI/index.js
@@ -0,0 +1,37 @@
+
+// 開始按鈕
+UI.startgame = {
+  Setting: {
+    name: 'startgame',
+    file: {
+      'main': { src: 'UI/start.png', w: 227, h: 74 },
+    },
+    scale: 1,
+  },
+  frame: {
+    0: {
+      pic: ['main', 0, 0], center: [0, 0], next: 0, wait: 1,
+    },
+
+  }
+};
+
+
+
+// LOGO
+UI.logo = {
+  Setting: {
+    name: 'logo',
+    file: {
+      'main': { src: 'UI/logo.png', w: 356, h: 84 },
+    },
+    scale: 2,
+  },
+  frame: {
+    0: {
+      pic: ['main', 0, 0], center: [0, 0], next: 0, wait: 1,
+    },
+
+  }
+};
+
