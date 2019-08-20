03bf378f53699ce7cf66c44e39c00191a1d7ab78
Ozen <komicabot@gmail.com>
2019-08-20 15:40:48 +0800

    修物理bug

--- features/physical.js
+++ features/physical.js
@@ -5,6 +5,10 @@ lf2.physical = (setting, frame, type, thing) => {
   // 計算物件速度
   lf2.move(setting, frame, type);
 
+  // 根據目前速度移動物件
+  setting.x += setting.xSpeed;
+  setting.y += setting.ySpeed;
+
   // UI固定位置
   if (type == 'UI') lf2.fixedPosition(setting, frame, type);
 
@@ -25,6 +29,8 @@ lf2.physical = (setting, frame, type, thing) => {
     // 滯空偵測
     setting.inSky = lf2.skyDetection(setting);
 
+    if (frame.falling && !setting.inSky) lf2.gotoFrame(thing, setting, type, 'lyingDown');
+
     // 翻轉允許
     if (frame.flip) {
       if (setting.keypress.right) setting.mirror = false;
@@ -32,9 +38,9 @@ lf2.physical = (setting, frame, type, thing) => {
     }
   }
 
-  // 根據目前速度移動物件
-  setting.x += setting.xSpeed;
-  setting.y += setting.ySpeed;
+
+
+
 }
 
 
@@ -47,9 +53,8 @@ lf2.move = (setting, frame, type) => {
     var m = setting.mirror ? -1 : 1;
 
     // 邊界偵測
-    if (lf2.mapDetection(setting, type))
+    if (!lf2.mapDetection(setting, type))
       setting.xSpeed = frame.move[0] * m;
-
     setting.ySpeed = frame.move[1];
   }
 
@@ -64,15 +69,15 @@ lf2.skyDetection = (setting, frame, type) => {
 }
 
 lf2.mapDetection = (setting, type) => {
-  var canGo = true;
+  var limit = false;
   if (type == 'derivative') {
     if (setting.x < -200 || setting.x > lf2.mainMap.limit.x + 200) setting.destroy = true;
   }
   else if (type == 'character') {
-    if ((setting.x >= 0 && setting.xSpeed < 0) || (setting.x < lf2.mainMap.limit.x && setting.xSpeed > 0)) canGo = false;
+    if ((setting.x <= 0 && setting.xSpeed < 0) || (setting.x >= lf2.mainMap.limit.x && setting.xSpeed > 0)) limit = true;
   }
 
-  return canGo;
+  return limit;
 }
 
 
